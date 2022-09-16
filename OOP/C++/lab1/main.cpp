e481195b4254671e5d8eda668ddb6c1a2c45231f
osipover <104246010+osipover@users.noreply.github.com>
2022-09-17 00:42:30 +0700

    Update main.cpp

--- OOP/C++/lab1/main.cpp
+++ OOP/C++/lab1/main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
-#include <string>
 #include "BigInt.h"
 using namespace std;
 
 int main() {
-    string BigIntInStr = "123456789123";
-    BigInt a(-3);
-    a = ~a;
+    BigInt a("-123");
+    BigInt b = a--;
+    b.Print();
     a.Print();
-    int b = -100;
-    b = ~(-100);
-    cout << b;
+
+    return 0;
 }
+
 
