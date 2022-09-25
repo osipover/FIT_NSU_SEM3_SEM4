e885ff3f58720d05f83adae7f0466f992931a191
osipover <104246010+osipover@users.noreply.github.com>
2022-09-25 14:50:37 +0700

    vector was replaced by list

--- OOP/C++/lab0/WordList.h
+++ OOP/C++/lab0/WordList.h
@@ -1,22 +1,29 @@
-#include <iostream>
-#include <vector>
+#include <list>
 #include <fstream>
 #include <string>
 using namespace std;
+
+typedef struct TStream{
+    ifstream input;
+    ofstream output;
+}TStream;
+
+TStream* OpenStream(const string& inputFile, const string& outputFile);
+void CloseStream(TStream* stream);
 
 typedef struct TWord {
 	string word;
 	int count;
 } TWord;
 
-class WordList {
-private:
-	vector<TWord*> list;
-    int countWords;
+class WordTable {
 public:
-	WordList(ifstream& file);
-	~WordList();
-	void AddWord(string word);
-	void Print();
+	WordTable(ifstream& input);
+	void AddWord(const string& word);
+	void Output(ofstream& output);
 	void Sort();
+    ~WordTable();
+private:
+    list<TWord*> table;
+    int countWords;
 };
