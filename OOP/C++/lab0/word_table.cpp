890e1059c2f128160be2b8affc1a7828296cfa4f
osipover <104246010+osipover@users.noreply.github.com>
2022-10-09 11:23:29 +0700

    Update word_table.cpp

--- OOP/C++/lab0/word_table.cpp
+++ OOP/C++/lab0/word_table.cpp
@@ -1,35 +1,37 @@
 #include "interface.h"
+#include <iostream>
 using namespace std;
 
-bool IsLetter(char symb) {
+bool isLetter(char symb) {
     if ((('a' <= symb) && (symb <= 'z')) || (('A' <= symb) && (symb <= 'Z')))
         return true;
     else
         return false;
 }
 
-bool CompareTwoWords(const string& word1, const string& word2) {
+bool compareTwoWords(const string& word1, const string& word2) {
     if (word1 == word2) return true;
     else return false;
 }
 
-pair<int, string> GetPairWithMaxCount(map<string, int> &table) {
-    auto cur = table.begin();
-    for (auto it = table.begin(); it != table.end(); ++it) {
-        if (cur->second < it->second) cur = it;
-    }
-    pair<int, string> maxPair(cur->second, cur->first);
-    table.erase(cur->first);
-    return maxPair;
-}
-
-multimap<int, string> SortTable(map<string, int> &table){
-    multimap<int, string> sortedTable;
-    while (!table.empty()){
-        auto cur = GetPairWithMaxCount(table);
-        sortedTable.insert(cur);
+multimap<int, const string*> sortTable(const map<string, int> &table){
+    multimap<int, const string*> sortedTable;
+    for (auto it = table.begin(); it != table.end(); ++it){
+        sortedTable.insert(pair<int, const string*>(it->second, &(it->first)));
     }
     return sortedTable;
+}
+
+void WordTable::addWord(const string& word) {
+    for (auto it = table.begin(); it != table.end(); ++it) {
+        if (compareTwoWords(it->first, word)) {
+            it->second++;
+            countWords++;
+            return;
+        }
+    }
+    table.insert(make_pair(word, 1));
+    countWords++;
 }
 
 WordTable::WordTable(const string& fileName) : countWords(0){
@@ -39,27 +41,25 @@ WordTable::WordTable(const string& fileName) : countWords(0){
     char symb = '0';
     input.get(symb);
     while (!input.eof()) {
-        while ((IsLetter(symb)) && (!input.eof())) {
+        while ((isLetter(symb)) && (!input.eof())) {
             word += symb;
             input.get(symb);
         }
         if (!word.empty()) {
-            AddWord(word);
+            addWord(word);
             word = "";
         }
         input.get(symb);
     }
-    sortedTable = SortTable(table);
+    sortedTable = sortTable(table);
 }
 
-void WordTable::AddWord(const string& word) {
-    for (auto it = table.begin(); it != table.end(); ++it) {
-        if (CompareTwoWords(it->first, word)) {
-            it->second++;
-            countWords++;
-            return;
-        }
-    }
-    table.insert(make_pair(word, 1));
-    countWords++;
+
+
+multimap<int, const string*>& WordTable:: getSortedTable(){
+    return sortedTable;
+}
+
+int WordTable::getCountWords() {
+    return countWords;
 }
