e77e52361ae7cd62bb0d33e79aee4782d5059273
osipover <104246010+osipover@users.noreply.github.com>
2022-10-09 11:22:33 +0700

    Update output.cpp

--- OOP/C++/lab0/output.cpp
+++ OOP/C++/lab0/output.cpp
@@ -1,19 +1,21 @@
 #include "interface.h"
 
-Output::Output(const string &fileName){
-    stream.open(fileName);
-}
-
-int CalcProcent(int num, int total) {
+int calcProcent(int num, int total) {
     float prop = (float) num / (float) total;
     int procent = (int) (prop * 100);
     return procent;
 }
 
-void Output::OutputData(const WordTable& table){
-    for (auto it = table.sortedTable.rbegin(); it != table.sortedTable.rend(); ++it){
-        stream << it->second << ';';
+Output::Output(const string &fileName){
+    stream.open(fileName);
+}
+
+void Output::outputData(WordTable& table){
+    auto sortedTable = table.getSortedTable();
+    for (auto it = sortedTable.rbegin(); it != sortedTable.rend(); ++it){
+        stream << *(it->second) << ';';
         stream << it->first << ';';
-        stream << CalcProcent(it->first, table.countWords) << endl;
+        stream << calcProcent(it->first, table.getCountWords())  << '%' << endl;
     }
 }
+
