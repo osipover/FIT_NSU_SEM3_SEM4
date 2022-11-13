9ce380f2f5ba1369e3fb7d628fdcbc1ec31a6e4c
osipover <104246010+osipover@users.noreply.github.com>
2022-11-13 17:06:30 +0700

    Update main.cpp

--- OOP/C++/lab2/main.cpp
+++ OOP/C++/lab2/main.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <string>
 #include "life.h"
 
 int main(int argc, char* argv[]) {
     Stream stream(argc, argv);
-    Galaxy galaxy;
-    galaxy.parc(stream.getInFile());
-    /*
-    Galaxy galaxy;
-    Field field(10, 10);
-    field.init();
-    while (true) {        
+    Galaxy galaxy(stream.getInFile());
+    Field field(galaxy);
+
+    int numIterat = stream.getNumIterat();
+    while (numIterat > 0) {
         field.output();
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
         system("cls");
         field.update(galaxy);
+        numIterat--;
     }
-    */
+
     return 0;
-    
 }
