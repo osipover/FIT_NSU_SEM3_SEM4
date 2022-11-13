c5133d80ee1e65f03b2c6aeb0015021c4ee3ca96
osipover <104246010+osipover@users.noreply.github.com>
2022-11-13 17:07:59 +0700

    Update life.h

--- OOP/C++/lab2/include/life.h
+++ OOP/C++/lab2/include/life.h
@@ -2,14 +2,16 @@
 #define LAB2_LIFE_H
 #include <fstream>
 #include <map>
+#include <vector>
 
 class Stream {
 public:
-    Stream(int argc, char** argv);
+    Stream(int argc, char* argv[]);
     void openInFile(const std::string& fileName);
     void openOutFile(const std::string& fileName);
     void close();
     std::ifstream& getInFile();
+    int getNumIterat();
 private:
     std::ifstream input;
     std::ofstream output;
@@ -19,10 +21,14 @@ private:
 class Galaxy {
 public:
     Galaxy();
-    void parc(std::ifstream& input);
+    Galaxy(std::ifstream& input);
+    std::vector<int>& getField();
+    int getHeight();
+    int getWidth();
     std::string getName();
     const std::map<int, int>& getBirthRules() const;
     const std::map<int, int>& getSurvivalRules() const;
+    std::vector<int> field;
 private:
     std::string name;
     int height;
@@ -31,24 +37,26 @@ private:
     std::map<int, int> survivalRules;
     void parcGalaxyName(std::string& line, std::ifstream& input);
     void parcRules(std::string& line, std::ifstream& input);
-    void parcFieldSize(std::string& line, std::ifstream& input);
+    void parcFieldSize(std::string& line, std::ifstream& input);  
+    std::vector<int> parcCells(std::string& line, std::ifstream& input);
+    void parc(std::ifstream& input);
 };
 
 class Field {
 public:
-    Field() : height(0), width(0), curField(nullptr), nextField(nullptr) {}
+    Field() : height(0), width(0) {}
     Field(int height, int width);
+    Field(Galaxy&);
     ~Field();
     int getHeight();
     int getWidth();
-    void init();
     void update(Galaxy& galaxy);
     void output();
 private:
     int height;
     int width;
-    int* curField;
-    int* nextField;
+    std::vector<int> curField;
+    std::vector<int> nextField;
 };
 
 class GameControler {
