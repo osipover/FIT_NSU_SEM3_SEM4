9e503518076ba23967e8ee9fb4a5bbd57f1e5927
osipover <104246010+osipover@users.noreply.github.com>
2022-11-27 00:38:10 +0700

    Update main.cpp

--- Computers & Peripheral devices/lab4/intrinsic/main.cpp
+++ Computers & Peripheral devices/lab4/intrinsic/main.cpp
@@ -1,2 +1,15 @@
-int main(){
+#include "matrix.h"
+#include <xmmintrin.h>
+#define SIZE 2048
+
+int main() {
+	std::ifstream input;
+	input.open("matrix.txt");
+	if (input.is_open()) {
+		Matrix A(SIZE);
+		A.init(input);
+		Matrix I = A.invert(10);
+	}
+
+	return 0;
 }
