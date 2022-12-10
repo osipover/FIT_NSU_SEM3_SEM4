379e9956c484dc00c419591aa1f8f73b8c343743
osipover <104246010+osipover@users.noreply.github.com>
2022-12-10 15:40:35 +0700

    Update pars.cpp

--- OOP/C++/lab2/lib/pars/src/pars.cpp
+++ OOP/C++/lab2/lib/pars/src/pars.cpp
@@ -2,11 +2,11 @@
 
 void Pars::commandLine(int argc, char* argv[]) {
     if (argc != 2) {
-        throw std::exception("[ERROR] No file for input or too many arguments.");
+        throw ParsExceptions("[ERROR] No file for input or too many arguments.");
     }
     this->input.open(argv[1]);
     if (!this->input.is_open()) {
-        throw std::exception("[ERROR] I can't open this file.");
+        throw ParsExceptions("[ERROR] I can't open this file.");
     }
 }
 
