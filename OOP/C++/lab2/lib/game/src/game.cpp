16f7bc77329df895d7241a3b0ea4b8c56277c95d
osipover <104246010+osipover@users.noreply.github.com>
2022-12-10 15:36:09 +0700

    Update game.cpp

--- OOP/C++/lab2/lib/game/src/game.cpp
+++ OOP/C++/lab2/lib/game/src/game.cpp
@@ -1,10 +1,45 @@
 #include <iostream>
-#include "field.h"
-#include "galaxy.h"
-#include "commands.h"
+#include <string>
+#include <sstream>
+#include "game.h"
+
+namespace {
+    std::vector<std::string> split(const std::string& line, char dev) {
+        std::vector<std::string> args;
+        std::stringstream stream;
+        stream.str(line);
+        std::string item;
+        while (std::getline(stream, item, dev)) {
+            args.push_back(item);
+        }
+        return args;
+    }
+
+    std::map<std::string, std::unique_ptr<ICommand>> createCommandList() {
+        std::map<std::string, std::unique_ptr<ICommand>> commandList;
+
+        auto creator = std::unique_ptr<ICommandCreator>{ new ExitCreator };
+        commandList.insert(std::pair<std::string, std::unique_ptr<ICommand>>("exit", creator->create()));
+
+        creator = std::unique_ptr<ICommandCreator>{ new HelpCreator };
+        commandList.insert(std::pair<std::string, std::unique_ptr<ICommand>>("help", creator->create()));
+
+        creator = std::unique_ptr<ICommandCreator>{ new TickCreator };
+        commandList.insert(std::pair<std::string, std::unique_ptr<ICommand>>("tick", creator->create()));
+
+        creator = std::unique_ptr<ICommandCreator>{ new PlayCreator };
+        commandList.insert(std::pair<std::string, std::unique_ptr<ICommand>>("play", creator->create()));
+
+        creator = std::unique_ptr<ICommandCreator>{ new DumpCreator };
+        commandList.insert(std::pair<std::string, std::unique_ptr<ICommand>>("dump", creator->create()));
+
+        return commandList;
+    }
+}
 
 void Game::start(Field& field, Galaxy& galaxy) {
-    std::map<std::string, std::unique_ptr<ICommand>> commandList = createCommandList();
+    auto commandList = createCommandList();
+
     while (true) {
         std::string line;
         std::getline(std::cin, line);
