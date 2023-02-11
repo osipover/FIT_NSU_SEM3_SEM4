e77e0a503018e2df3872fd05357fb381545aa43e
osipover <104246010+osipover@users.noreply.github.com>
2023-02-11 18:58:54 +0700

    Update and rename Parser.java to FileParser.java

--- OOP/Java/lab1/FileParser.java
+++ OOP/Java/lab1/FileParser.java
@@ -1,27 +1,27 @@
 import java.io.IOException;
 import java.io.Reader;
 
-public class Parser {
-    Parser(Reader reader){
+public class FileParser {
+    FileParser(Reader reader){
         this.reader = reader;
     }
-    
+
     public String getNextWord() throws IOException {
         StringBuilder newWord = new StringBuilder("");
         try{
-            while(newWord.toString().equals("")){
-                //System.out.println("!");
-                int data = reader.read();
+            int data = 0;
+            while(newWord.toString().equals("") && (data != -1)){
+                data = reader.read();
                 while((data != -1) && Character.isLetterOrDigit(data)){
                     newWord.append((char)data);
                     data = reader.read();
                 }
             }
+            return newWord.toString();
         }
         catch(IOException ex){
             throw ex;
         }
-        return newWord.toString();
     }
 
     private Reader reader;
