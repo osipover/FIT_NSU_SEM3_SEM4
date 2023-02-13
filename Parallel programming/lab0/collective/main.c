a2787b84271440c12703ef9c4634f3afb47efd0c
Alexander Osipov <104246010+osipover@users.noreply.github.com>
2023-02-13 20:46:18 +0700

    Create main.c

---
+++ Parallel programming/lab0/collective/main.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "mpi.h"
+#define SIZE 100000
+#define ID 1
+
+int CalcCurrentArraySize(int numProc) {
+	if (SIZE % (numProc) == 0) 
+		return SIZE;
+	else
+		return SIZE + (numProc) - (SIZE % (numProc));
+}
+
+void InitArray(int* arr, int curArraySize) {
+	for (int i = 0; i < curArraySize; ++i) {
+		arr[i] = (i < SIZE ? i : 0);
+	}
+}
+
+long long Mult(int* a, int sizeA, int* b, int sizeB) {
+	long long result = 0;
+	for (int i = 0; i < sizeA; ++i) {
+		for (int j = 0; j < sizeB; ++j) {
+			result += a[i] * b[j];
+		}
+	}
+	return result;
+}
+
+void FreeArrays(int* array1, int* array2, int* recvBuffer) {
+	free(array1);
+	free(array2);
+	free(recvBuffer);
+}
+
+int main(int argc, char** argv) {
+	int numProc, rank;
+	MPI_Status status;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &numProc);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	const int curArraySize = CalcCurrentArraySize(numProc);
+	int* array1 = malloc(curArraySize * sizeof(int));
+	int* array2 = malloc(curArraySize * sizeof(int));
+
+	double timeStart, timeEnd;
+	if (rank == 0) {
+		InitArray(array1, curArraySize);
+		InitArray(array2, curArraySize);
+		timeStart = MPI_Wtime();
+	}
+
+	long long s = 0, stmp = 0;	
+	const int bufferSize = curArraySize / (numProc);
+	int* recvBuffer = malloc(bufferSize * sizeof(int));
+
+	MPI_Scatter(array1, bufferSize, MPI_INT, recvBuffer, bufferSize, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(array2, SIZE, MPI_INT, 0, MPI_COMM_WORLD);
+	stmp = Mult(recvBuffer, bufferSize, array2, SIZE);
+	MPI_Reduce(&stmp, &s, 1, MPI_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+	
+	if (rank == 0) {
+		timeEnd = MPI_Wtime();
+		printf("S = %lld\n", s);
+		printf("Total time: %f sec\n", timeEnd - timeStart);
+	}
+	FreeArrays(array1, array2, recvBuffer);
+	MPI_Finalize();
+
+	return 0;
+}
