e745fcb7622dd1354805a195bfcfd1c9d80d4ba2
Alexander Osipov <104246010+osipover@users.noreply.github.com>
2023-02-15 17:27:56 +0700

    Update WordStatistic.java

--- OOP/Java/lab1/WordStatistic.java
+++ OOP/Java/lab1/WordStatistic.java
@@ -1,28 +1,24 @@
 public class WordStatistic {
+    public static int totalCount = 0;
+    private final String word;
+    private final int numInclusion;
+
     public WordStatistic (String word, int numInclusion){
         this.word = word;
         this.numInclusion = numInclusion;
     }
-
     public String getWord(){
         return this.word;
     }
-
     public int getNumInclusion(){
         return this.numInclusion;
     }
-
     @Override
     public String toString(){
         return word + ";" + numInclusion + ";" + percentOfAllWords() + "%";
     }
-
     private double percentOfAllWords(){
         double percent = ((double)numInclusion/totalCount) * 100;
         return (double)((int)(percent * 100))/100;
     }
-
-    public static int totalCount = 0;
-    private String word;
-    private int numInclusion;
 }
