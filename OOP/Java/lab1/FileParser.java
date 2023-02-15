6b25812e14e4cce1799aebb58cde79a2a4ecdebd
Alexander Osipov <104246010+osipover@users.noreply.github.com>
2023-02-15 17:26:53 +0700

    Update FileParser.java

--- OOP/Java/lab1/FileParser.java
+++ OOP/Java/lab1/FileParser.java
@@ -2,27 +2,21 @@ import java.io.IOException;
 import java.io.Reader;
 
 public class FileParser {
+    private final Reader reader;
+
     FileParser(Reader reader){
         this.reader = reader;
     }
-
     public String getNextWord() throws IOException {
         StringBuilder newWord = new StringBuilder("");
-        try{
-            int data = 0;
-            while(newWord.toString().equals("") && (data != -1)){
+        int data = 0;
+        while(newWord.toString().equals("") && (data != -1)){
+            data = reader.read();
+            while(Character.isLetterOrDigit(data)){
+                newWord.append((char)data);
                 data = reader.read();
-                while((data != -1) && Character.isLetterOrDigit(data)){
-                    newWord.append((char)data);
-                    data = reader.read();
-                }
             }
-            return newWord.toString();
         }
-        catch(IOException ex){
-            throw ex;
-        }
+        return newWord.toString();
     }
-
-    private Reader reader;
 }
