cc96daadff9d664fb140a85bbfb4c29723fa6596
Alexander Osipov <104246010+osipover@users.noreply.github.com>
2023-03-31 23:44:09 +0700

    Update main.s

--- Computers & Peripheral devices/lab2/O3/main.s
+++ Computers & Peripheral devices/lab2/O3/main.s
@@ -1,20 +1,20 @@
 PiByGregoryLeibniz:
-        testq   %rdi, %rdi 			      //N: %rdi
-        jle     .L4				            //если N==1, то переходим по .L4
-        movsd   .LC0(%rip), %xmm1		  //pow = -1: %xmm1
-        leaq    3(%rdi,%rdi), %rdx	  //N: %rdx
+        testq   %rdi, %rdi 			        //N: %rdi
+        jle     .L4				        //если N==1, то переходим по .L4
+        movsd   .LC0(%rip), %xmm1		        //pow = -1: %xmm1
+        leaq    3(%rdi,%rdi), %rdx	                //N: %rdx
         movl    $3, %eax			
-        movsd   .LC1(%rip), %xmm0		  //pi = 1.0: %xmm0
+        movsd   .LC1(%rip), %xmm0		        //pi = 1.0: %xmm0
         movq    .LC3(%rip), %xmm4
 .L3:  
 //В этом блоке реализован цикл в функции PiByGregoryLeibniz
-        pxor    %xmm3, %xmm3		      //(%xmm3) = 0
-        movapd  %xmm1, %xmm2		      //pow: %xmm2
+        pxor    %xmm3, %xmm3		                //(%xmm3) = 0
+        movapd  %xmm1, %xmm2		                //pow: %xmm2
         xorpd   %xmm4, %xmm1		
         cvtsi2sdq       %rax, %xmm3
         addq    $2, %rax			
-        divsd   %xmm3, %xmm2		      //pow /= (2*i+1)
-        addsd   %xmm2, %xmm0		      //pi += pow/(2*i+1)
+        divsd   %xmm3, %xmm2		                //pow /= (2*i+1)
+        addsd   %xmm2, %xmm0		                //pi += pow/(2*i+1)
         cmpq    %rax, %rdx
         jne     .L3
 //Выход из функции
@@ -31,13 +31,13 @@ PiByGregoryLeibniz:
 
 TestClockGettime:
         pushq   %rbx				
-        movq    %rdi, %rbx			      //N: %rbx
+        movq    %rdi, %rbx			        //N: %rbx
         movl    $4, %edi
         subq    $48, %rsp
-        leaq    16(%rsp), %rsi		    //start: 16(%rsp)
+        leaq    16(%rsp), %rsi		                 //start: 16(%rsp)
         call    clock_gettime
         testq   %rbx, %rbx			
-        jle     .L10				          //если N==1, то переходим в .L10
+        jle     .L10				        //если N==1, то переходим в .L10
 //Ниже представлен тот же набор команд, что и функции PiByGregoryLeibniz
   movsd   .LC1(%rip), %xmm0		
         leaq    3(%rbx,%rbx), %rdx
@@ -61,24 +61,24 @@ TestClockGettime:
 
 .L8:	
 //Этот блок отвечает за вызов функции clock_gettime
-        leaq    32(%rsp), %rsi		    //end: 32(%rsp)
+        leaq    32(%rsp), %rsi		                //end: 32(%rsp)
         movl    $4, %edi
         movsd   %xmm0, 8(%rsp)
         call    clock_gettime
 //Работа команды printf для Pi
-        movsd   8(%rsp), %xmm0		    //pi: %xmm0
+        movsd   8(%rsp), %xmm0		                //pi: %xmm0
         movl    $.LC4, %edi
         movl    $1, %eax
         call    printf
 //Реализуем (end.tv_nsec - start.tv_nsec)
-        movq    40(%rsp), %rax		    //end.tv_nsec: 40(%rsp)
+        movq    40(%rsp), %rax		                //end.tv_nsec: 40(%rsp)
         pxor    %xmm0, %xmm0
-        subq    24(%rsp), %rax		    //start.tv_nsec: 24(%rsp)
+        subq    24(%rsp), %rax		                //start.tv_nsec: 24(%rsp)
         cvtsi2sdq       %rax, %xmm0
 //Реализуем (end.tv_sec - start.tv_sec)
         pxor    %xmm1, %xmm1
-        movq    32(%rsp), %rax		    //end.tv_sec: 32(%rsp)
-        subq    16(%rsp), %rax		    //start.tv_sec: 16(%rsp)
+        movq    32(%rsp), %rax		                //end.tv_sec: 32(%rsp)
+        subq    16(%rsp), %rax		                //start.tv_sec: 16(%rsp)
 //Реализуем 0.000000001*(end.tv_nsec - start.tv_nsec)
         mulsd   .LC5(%rip), %xmm0
 //Реализуем end.tv_sec - start.tv_sec + 0.000000001*(end.tv_nsec - start.tv_nsec)
@@ -93,7 +93,7 @@ TestClockGettime:
         ret
 
 .L10:	//Условный переход, если N==1
-        movsd   .LC2(%rip), %xmm0 	  //pi = 4: %xmm0
+        movsd   .LC2(%rip), %xmm0 	                //pi = 4: %xmm0
         jmp     .L8
 
 main:
