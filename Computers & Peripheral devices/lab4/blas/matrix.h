174c5bc632e32eb5969b737cbe8c4352bd91fffc
Alexander Osipov <104246010+osipover@users.noreply.github.com>
2023-04-01 00:02:08 +0700

    Create matrix.h

---
+++ Computers & Peripheral devices/lab4/blas/matrix.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+class Matrix {
+public:
+	Matrix();
+	Matrix(int N);
+	void init(std::ifstream& input);
+	void output();
+	int getN();
+	float getCell(int i, int j);
+	void setCell(int i, int j, float value);	
+	Matrix transpose();
+	Matrix invert(int M);
+	Matrix& operator=(const Matrix& right);
+	Matrix& operator+=(const Matrix& right);
+	Matrix& operator-=(const Matrix& right);
+	friend Matrix operator*(const Matrix& left, const Matrix& right);
+	friend Matrix operator+(const Matrix& left, const Matrix& right);
+	friend Matrix operator-(const Matrix& left, const Matrix& right);
+	friend Matrix operator/(const Matrix& left, float right);
+private:	
+	float* matrix;	
+	size_t N;
+};
