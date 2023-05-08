19898810ee9992e263b5670aee3bde70cf32334a
Alexander Osipov <104246010+osipover@users.noreply.github.com>
2023-05-08 14:33:56 +0700

    Create main.c

---
+++ OS/Process address space/Address space structure/2/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int* get_pointer_v1() { /* */
+    int a = 10;
+    return &a;
+}
+
+int* get_pointer_v2() { /* */
+    int a = 10;
+    int *pa = &a;
+    return pa;
+}
+
+int main(int argc, char** argv) {
+    int* ptr1 = get_pointer_v1();
+    printf("ptr1: %p\n", ptr1);     //ptr1: (nil)   
+    printf("*ptr1 = %d\n", *ptr1);  //segmentation fault
+  
+    int* ptr2 = get_pointer_v2();   
+    printf("ptr2: %p\n", ptr2);     //ptr1: 0x7fff6d14e94c   
+    printf("*ptr2 = %d\n", *ptr2);  //undefined behavior 
+}
