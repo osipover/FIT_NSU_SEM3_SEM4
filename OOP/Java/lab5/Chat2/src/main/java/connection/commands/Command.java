f7e64cb7d3085111bed04b427856abcb14486c4c
Alexander Osipov <104246010+osipover@users.noreply.github.com>
2023-06-04 10:12:45 +0700

    Delete OOP/Java/lab5/Chat2 directory

--- OOP/Java/lab5/Chat2/src/main/java/connection/commands/Command.java
+++
@@ -1,8 +0,0 @@
-package connection.commands;
-
-import connection.Connection;
-import connection.packet.Packet;
-
-public interface Command {
-    void execute(Connection connection, Packet message);
-}
